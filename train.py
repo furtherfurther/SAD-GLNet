b1af537776c3956a5db93f2eda523cf1166f09e1
lhb <91206041+furtherfurther@users.noreply.github.com>
2026-01-10 16:33:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -16,28 +16,29 @@ from vision import confuPLT, sampleCount
 
 
 def get_train_valid_sampler(trainset, valid=0.1, dataset='MELD'):
-    """用于创建训练集和验证集的采样器
-    这个函数特别适用于在训练深度学习模型时进行模型的验证
+    """Create samplers for training and validation sets.
+    This function is particularly useful for model validation during deep learning model training.
     """
     size = len(trainset)
     idx = list(range(size))
-    # 计算验证集的大小，根据提供的比例 valid 来确定
+    # Calculate validation set size based on the provided valid ratio
     split = int(valid*size)
-    # 返回两个 SubsetRandomSampler 对象，分别用于训练集和验证集
+    # Return two SubsetRandomSampler objects for training and validation sets respectively
     return SubsetRandomSampler(idx[split:]), SubsetRandomSampler(idx[:split])
 
 
 def get_MELD_loaders(batch_size=32, valid=0.1, num_workers=0, pin_memory=False):
     """
-    用于创建 MELD 数据集的训练、验证和测试加载器（loaders）
-   这些加载器使用了 DataLoader 类，并且可以指定批次大小、是否使用多线程加载数据、是否固定内存等参数。
-    :param num_workers:加载数据时使用的子进程数量，默认为 0
-    :param pin_memory:是否将数据加载到固定的内存中，以加速数据转移到 CUDA 设备上的速度，默认为 False
-    :return:返回包含训练、验证和测试加载器的元组
+    Create data loaders for MELD dataset training, validation, and testing.
+    These loaders use the DataLoader class and can specify batch size, whether to use multi-threaded data loading, 
+    whether to pin memory, and other parameters.
+    :param num_workers: Number of subprocesses to use for data loading, default is 0
+    :param pin_memory: Whether to load data into pinned memory to accelerate data transfer to CUDA devices, default is False
+    :return: Returns a tuple containing training, validation, and testing loaders
     """
     trainset = MELDDataset('data/meld_multimodal_features.pkl')
     train_sampler, valid_sampler = get_train_valid_sampler(trainset, valid, 'MELD')
-    # 创建训练集的 DataLoader，使用指定的批次大小、采样器、collate 函数、子进程数量和固定内存选项
+    # Create DataLoader for training set with specified batch size, sampler, collate function, number of workers, and pin_memory option
     train_loader = DataLoader(trainset,
                               batch_size=batch_size,
                               sampler=train_sampler,
@@ -57,7 +58,7 @@ def get_MELD_loaders(batch_size=32, valid=0.1, num_workers=0, pin_memory=False):
                              collate_fn=testset.collate_fn,
                              num_workers=num_workers,
                              pin_memory=pin_memory)
-    # 返回包含训练、验证和测试加载器的元组
+    # Return a tuple containing training, validation, and testing loaders
     return train_loader, valid_loader, test_loader
 
 
@@ -88,35 +89,36 @@ def get_IEMOCAP_loaders(batch_size=32, valid=0.1, num_workers=0, pin_memory=Fals
 
 def train_or_eval_model(model, loss_function, kl_loss, dataloader, epoch, optimizer=None, train=False, gamma_1=1.0, gamma_2=1.0, gamma_3=1.0):
     """
-    用于训练或评估一个模型，具体取决于 train 参数的值。它处理数据加载、前向传播、损失计算、反向传播（如果是训练模式）以及性能评估
-    :param kl_loss:用于计算KL散度损失的函数
-    :param train:一个布尔值，指示是训练模型还是评估模型
-    :param gamma_1:用于加权不同损失项的系数
+    Train or evaluate a model, depending on the value of the train parameter. 
+    It handles data loading, forward propagation, loss calculation, backpropagation (if in training mode), and performance evaluation.
+    :param kl_loss: Function for calculating KL divergence loss
+    :param train: Boolean value indicating whether to train the model or evaluate the model
+    :param gamma_1: Coefficient for weighting different loss terms
     """
     losses, preds, labels, masks = [], [], [], []
     labels_g = []
-    # 确保在训练模式下提供了优化器
+    # Ensure optimizer is provided in training mode
     assert not train or optimizer!=None
     if train:
         model.train()
     else:
         model.eval()
 
-    # 遍历 dataloader 中的数据
+    # Iterate through data in dataloader
     for data in dataloader:
-        # 如果是训练模式，清除优化器的梯度
+        # If in training mode, clear optimizer gradients
         if train:
             optimizer.zero_grad()
-        # 将数据转移到设备上（如果使用 GPU，则为 CUDA 设备），并准备数据
-        # 可以猜测数据集的特征是textf, visuf, acouf, qmask, umask, label，最后一个维度是label
+        # Move data to device (CUDA device if using GPU) and prepare data
+        # Can guess dataset features are textf, visuf, acouf, qmask, umask, label, with the last dimension being label
         textf, visuf, acouf, qmask, umask, label = [d.cuda() for d in data[:-1]] if cuda else data[:-1]
         # qmask = qmask.permute(1, 0, 2)
         lengths = [(umask[j] == 1).nonzero().tolist()[-1][0] + 1 for j in range(len(umask))]
-        # 调用模型进行前向传播，获取不同模态和多模态融合的对数概率和概率
+        # Call model for forward propagation, get log probabilities and probabilities for different modalities and multimodal fusion
         log_prob1, log_prob2, log_prob3, all_log_prob, all_prob, \
         kl_log_prob1, kl_log_prob2, kl_log_prob3, kl_all_prob = model(textf, visuf, acouf, umask, qmask, lengths)
 
-        # 计算主损失和KL散度损失，并将它们组合成总损失
+        # Calculate main loss and KL divergence loss, and combine them into total loss
         lp_1 = log_prob1.view(-1, log_prob1.size()[2])
         lp_2 = log_prob2.view(-1, log_prob2.size()[2])
         lp_3 = log_prob3.view(-1, log_prob3.size()[2])
@@ -158,18 +160,16 @@ def train_or_eval_model(model, loss_function, kl_loss, dataloader, epoch, optimi
                 gamma_2 * (loss_function_g(lp_1, labels_) + loss_function_g(lp_2, labels_) + loss_function_g(lp_3, labels_)) + \
                 gamma_3 * (kl_loss(kl_lp_1, kl_p_all) + kl_loss(kl_lp_2, kl_p_all) + kl_loss(kl_lp_3, kl_p_all))
 
-      
-
         lp_ = all_prob.view(-1, all_prob.size()[1])
         # print("lp_", lp_.shape)  # [718, 6])
-        # 将预测的概率转换为预测标签
+        # Convert predicted probabilities to predicted labels
         pred_ = torch.argmax(lp_, 1)
         # print("pred_", pred_.shape)  # ([718])
         # print("labels_", labels_.shape)  # ([1760])
         # print("label_g", label_g.shape)  # ([718])
         # print("labels", labels)
 
-        # 将损失、预测、标签和掩码添加到相应的列表中
+        # Add loss, predictions, labels, and masks to respective lists
         preds.append(pred_.data.cpu().numpy())
         labels.append(labels_.data.cpu().numpy())
         labels_g.append(label_g.data.cpu().numpy())
@@ -177,7 +177,7 @@ def train_or_eval_model(model, loss_function, kl_loss, dataloader, epoch, optimi
         masks.append(umask.view(-1).cpu().numpy())
         losses.append(loss.item()*masks[-1].sum())
 
-        # 如果是训练模式，执行反向传播并更新模型参数
+        # If in training mode, perform backpropagation and update model parameters
         if train:
             loss.backward()
             if args.tensorboard:
@@ -193,47 +193,47 @@ def train_or_eval_model(model, loss_function, kl_loss, dataloader, epoch, optimi
     else:
         return float('nan'), float('nan'), [], [], [], float('nan')
 
-    # 计算平均损失、准确率和 F1 分数
+    # Calculate average loss, accuracy, and F1 score
     avg_loss = round(np.sum(losses)/np.sum(masks), 4)
     # print("labels", labels.shape)  # (9512,)
-    # print("preds", preds.shape)  # (5810,) 维度不一致无法预测，需要维度一致
+    # print("preds", preds.shape)  # (5810,) Dimension mismatch for prediction, dimensions need to be consistent
     # print("label_g", label_g.shape)  # ([718])
     # print("masks", masks.shape)  # (9512,)
     # avg_accuracy = round(accuracy_score(labels, preds, sample_weight=masks)*100, 2)
     # avg_fscore = round(f1_score(labels, preds, sample_weight=masks, average='weighted')*100, 2)
     avg_accuracy = round(accuracy_score(labels_g, preds) * 100, 2)
     avg_fscore = round(f1_score(labels_g, preds, average='weighted') * 100, 2)
-    # 返回计算得到的平均损失、准确率、标签、预测、掩码和 F1 分数
+    # Return calculated average loss, accuracy, labels, predictions, masks, and F1 score
     # return avg_loss, avg_accuracy, labels, preds, masks, avg_fscore
     return avg_loss, avg_accuracy, labels_g, preds, masks, avg_fscore
 
 
 if __name__ == '__main__':
-    """Python 脚本，它使用 argparse 库来解析命令行参数
-    通过这种方式，用户可以轻松地调整模型的超参数，而无需修改代码本身。
+    """Python script that uses the argparse library to parse command line arguments.
+    This way, users can easily adjust model hyperparameters without modifying the code itself.
     """
-    # 用于处理命令行参数
+    # For handling command line arguments
     parser = argparse.ArgumentParser()
-    # 向解析器添加参数。每个参数都有一些属性，如动作（action）、默认值（default）、类型（type）、帮助信息（help）等
+    # Add parameters to the parser. Each parameter has some attributes like action, default value, type, help information, etc.
     parser.add_argument('--no-cuda', action='store_true', default=False, help='does not use GPU')
     parser.add_argument('--lr', type=float, default=0.0001, metavar='LR', help='learning rate')
     parser.add_argument('--l2', type=float, default=0.00001, metavar='L2', help='L2 regularization weight')
     parser.add_argument('--dropout', type=float, default=0.5, metavar='dropout', help='dropout rate')
     parser.add_argument('--batch-size', type=int, default=16, metavar='BS', help='batch size')  # 16->2
     parser.add_argument('--hidden_dim', type=int, default=1024, metavar='hidden_dim', help='output hidden size')
-    parser.add_argument('--n_head', type=int, default=8, metavar='n_head', help='number of heads')  # 多头注意力机制中的头数
+    parser.add_argument('--n_head', type=int, default=8, metavar='n_head', help='number of heads')  # Number of heads in multi-head attention mechanism
     parser.add_argument('--epochs', type=int, default=80, metavar='E', help='number of epochs')
-    parser.add_argument('--temp', type=int, default=1.0, metavar='temp', help='temp')  # 温度参数，通常用于调整 softmax 的输出
+    parser.add_argument('--temp', type=int, default=1.0, metavar='temp', help='temp')  # Temperature parameter, typically used to adjust softmax output
     parser.add_argument('--tensorboard', action='store_true', default=False, help='Enables tensorboard log')
     parser.add_argument('--class-weight', action='store_true', default=True, help='use class weights')
     parser.add_argument('--Dataset', default='IEMOCAP', help='dataset to train and test')  # IEMOCAP MELD
 
-    # 解析命令行参数，并将它们存储在 args 变量中
+    # Parse command line arguments and store them in the args variable
     args = parser.parse_args()
     today = datetime.datetime.now()
     print(args)
 
-    # 根据系统是否支持 CUDA 以及用户是否指定不使用 CUDA 来设置 cuda 标志
+    # Set cuda flag based on whether the system supports CUDA and whether the user specifies not to use CUDA
     args.cuda = torch.cuda.is_available() and not args.no_cuda
     if args.cuda:
         print('Running on GPU')
@@ -247,7 +247,7 @@ if __name__ == '__main__':
     cuda = args.cuda
     n_epochs = args.epochs
     batch_size = args.batch_size
-    # 根据数据集的不同，设置音频、视觉和文本特征的维度
+    # Set audio, visual, and text feature dimensions based on the dataset
     feat2dim = {'IS10': 1582, 'denseface': 342, 'MELD_audio': 300}
     D_audio = feat2dim['IS10'] if args.Dataset=='IEMOCAP' else feat2dim['MELD_audio']
     D_visual = feat2dim['denseface']
@@ -255,24 +255,18 @@ if __name__ == '__main__':
 
     D_m = D_audio + D_visual + D_text
 
-    # 根据数据集的不同，设置说话者数量和类别数量
+    # Set number of speakers and number of classes based on the dataset
     n_speakers = 9 if args.Dataset=='MELD' else 2
     n_classes = 7 if args.Dataset=='MELD' else 6 if args.Dataset=='IEMOCAP' else 1
-    # 打印温度参数
+    # Print temperature parameter
     print('temp {}'.format(args.temp))
 
-    # 2. 这段代码是训练和评估过程的准备阶段，它确保模型、损失函数和优化器都已正确设置，并且数据加载器已经准备好提供训练和验证数据。
+    # 2. This code is the preparation stage for training and evaluation process, ensuring that the model, loss function, and optimizer are correctly set, and data loaders are ready to provide training and validation data.
     model = Transformer_Based_Model(args.Dataset, args.temp, D_text, D_visual, D_audio, args.n_head,
                                         n_classes=n_classes,
                                         hidden_dim=args.hidden_dim,
                                         n_speakers=n_speakers,
                                         dropout=args.dropout)
-
-    total_params = sum(p.numel() for p in model.parameters())
-    print('total parameters: {}'.format(total_params))
-    # 计算模型中可训练的参数数量
-    total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print('training parameters: {}'.format(total_trainable_params))
 
 
     def get_model_size(model):
@@ -284,9 +278,9 @@ if __name__ == '__main__':
         }
 
         total_params = sum(p.numel() for p in model.parameters())
-        # 获取模型中参数的 dtype（默认取第一个参数的 dtype）
+        # Get dtype of model parameters (default is to take dtype of first parameter)
         param_dtype = next(model.parameters()).dtype
-        bytes_per_param = dtype_size_map.get(param_dtype, 4)  # 默认为 float32
+        bytes_per_param = dtype_size_map.get(param_dtype, 4)  # Default is float32
 
         param_size_bytes = total_params * bytes_per_param
         param_size_mb = param_size_bytes / (1024 * 1024)
@@ -297,18 +291,18 @@ if __name__ == '__main__':
         return total_params, param_size_mb
 
 
-    # 使用示例
+    # Usage example
     get_model_size(model)
 
     if cuda:
-        # 将模型转移到 GPU 上
+        # Move model to GPU
         model.cuda()
         
     # kl_loss = MaskedKLDivLoss()
     kl_loss = KLDivLoss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.l2)
 
-    # 根据数据集的不同，设置损失函数和数据加载器
+    # Set loss function and data loaders based on the dataset
     if args.Dataset == 'MELD':
         loss_function = MaskedNLLLoss()
         train_loader, valid_loader, test_loader = get_MELD_loaders(valid=0.0,
@@ -327,25 +321,25 @@ if __name__ == '__main__':
     else:
         print("There is no such dataset")
 
-    # 用于跟踪验证过程中的最佳性能
+    # For tracking best performance during validation
     best_fscore, best_loss, best_label, best_pred, best_mask, best_label2 = None, None, None, None, None, None
-    # 用于存储训练过程中的性能记录
+    # For storing performance records during training
     all_fscore, all_acc, all_loss = [], [], []
 
-    # 3. 这段代码是一个训练循环，用于训练和评估一个基于 Transformer 的模型。它遍历指定的训练周期（epochs），在每个周期中进行训练、验证和测试，并记录性能指标
+    # 3. This code is a training loop for training and evaluating a Transformer-based model. It iterates through the specified number of epochs, performing training, validation, and testing in each epoch, and records performance metrics.
     for e in range(n_epochs):
         start_time = time.time()
 
-        # 训练
+        # Training
         train_loss, train_acc, _, _, _, train_fscore = train_or_eval_model(model, loss_function, kl_loss, train_loader, e, optimizer, True)
-        # 验证
+        # Validation
         valid_loss, valid_acc, _, _, _, valid_fscore = train_or_eval_model(model, loss_function, kl_loss, valid_loader, e)
-        # 测试
+        # Testing
         test_loss, test_acc, test_label, test_pred, test_mask, test_fscore = train_or_eval_model(model, loss_function, kl_loss, test_loader, e)
         all_fscore.append(test_fscore)
         all_acc.append(test_acc)
 
-        # 如果这是第一次迭代或者当前测试 F-Score 高于之前的最佳值，则更新最佳 F-Score 和相应的标签和预测
+        # If this is the first iteration or the current test F-Score is higher than the previous best, update best F-Score and corresponding labels and predictions
         if best_fscore == None or best_fscore < test_fscore:
             best_fscore = test_fscore
             best_label, best_pred, best_mask = test_label, test_pred, test_mask
@@ -359,7 +353,7 @@ if __name__ == '__main__':
         print('epoch: {}, train_loss: {}, train_acc: {}, train_fscore: {}, valid_loss: {}, valid_acc: {}, valid_fscore: {}, test_loss: {}, test_acc: {}, test_fscore: {}, time: {} sec'.\
                 format(e+1, train_loss, train_acc, train_fscore, valid_loss, valid_acc, valid_fscore, test_loss, test_acc, test_fscore, round(time.time()-start_time, 2)))
         if (e+1) % 10 == 0:
-            # 打印最佳标签和预测的分类报告   和混淆矩阵
+            # Print classification report and confusion matrix for best labels and predictions
             # print(classification_report(best_label, best_pred, sample_weight=best_mask, digits=4))
             # print(confusion_matrix(best_label, best_pred, sample_weight=best_mask))
             print(classification_report(best_label, best_pred, digits=4))
@@ -374,45 +368,9 @@ if __name__ == '__main__':
     # print('ACC: {}'.format(max(all_acc)))
     print('index: {}'.format(all_fscore.index(max(all_fscore)) + 1))
 
-    # 4. 这段代码是用来记录模型的性能指标到一个文件中，以便后续分析和比较。它使用了 Python 的 pickle 模块来序列化和反序列化数据
-    # 检查指定日期的记录文件是否存在
-    if not os.path.exists("record_{}_{}_{}.pk".format(today.year, today.month, today.day)):
-        # 如果文件不存在，打开文件以写入二进制模式
-        with open("record_{}_{}_{}.pk".format(today.year, today.month, today.day),'wb') as f:
-            # 将一个空字典序列化到文件中
-            pk.dump({}, f)
-    # 打开文件以读取二进制模式
-    with open("record_{}_{}_{}.pk".format(today.year, today.month, today.day), 'rb') as f:
-        record = pk.load(f)
 
-    # 定义一个键，用于在 record 字典中存储数据
-    key_ = 'name_'
-    if record.get(key_, False):
-        # 如果键存在，将最高的 F-Score 添加到对应的列表中
-        record[key_].append(max(all_fscore))
-    else:
-        # 如果键不存在，创建一个新的列表，并添加最高的 F-Score
-        record[key_] = [max(all_fscore)]
 
-    if record.get(key_+'record', False):
-        # 如果键存在，将分类报告添加到对应的列表中
-        # record[key_+'record'].append(classification_report(best_label, best_pred, sample_weight=best_mask,digits=4))
-        record[key_ + 'record'].append(classification_report(best_label, best_pred, digits=4))
-    else:
-        # 如果键不存在，创建一个新的列表，并添加分类报告
-        # record[key_+'record'] = [classification_report(best_label, best_pred, sample_weight=best_mask,digits=4)]
-        record[key_ + 'record'] = [classification_report(best_label, best_pred, digits=4)]
-    # 再次打开文件以写入二进制模式
-    with open("record_{}_{}_{}.pk".format(today.year, today.month, today.day),'wb') as f:
-        # 将更新后的 record 字典序列化回文件
-        pk.dump(record, f)
-
-    # 打印分类报告和混淆矩阵
-    # print(classification_report(best_label, best_pred, sample_weight=best_mask, digits=4))
-    # print(confusion_matrix(best_label, best_pred, sample_weight=best_mask))
+  
 
     print(classification_report(best_label, best_pred, digits=4))
     print(confusion_matrix(best_label, best_pred))
-  
-
-
